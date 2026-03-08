7786923412ca0a27e6de9253f99675f0b42040d9
Antara Thapliyal <thapliyalantara31@gmail.com>
2026-03-07 19:44:24 -0500

    toggles working

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,8 @@ from flask_cors import CORS
 from dotenv import load_dotenv
 import os
 import requests
+from flask import send_from_directory
+
 
 load_dotenv()
 token=None
@@ -13,13 +15,34 @@ password=os.getenv("NJT_PWD")
 token=os.getenv("NJT_TOKEN")
 app=Flask(__name__)
 CORS(app)
+print("TOKEN FROM ENV:", os.getenv("NJT_TOKEN"))
+# response = requests.post("https://raildata.njtransit.com/api/TrainData/getToken", data={"username": username, "password": password})
+# print(response.json())
+# token = response.json().get("token")
+
+@app.route("/")
+def index():
+    return send_from_directory("../frontend", "index.html")
+
 @app.route("/trains/<station_name>")
 def get_trains(station_name):
     your_station_lst=[]
-    response1=requests.post("https://testraildata.njtransit.com/api/TrainData/getTrainSchedule19Rec",data={"token":token,"station":station_name})
+    response1=requests.post("https://raildata.njtransit.com/api/TrainData/getTrainSchedule19Rec",data={"token":token,"station":station_name})
     response1=response1.json()
     response1=response1["ITEMS"]
     return jsonify(response1)
+
+@app.route("/trains/stops/<train>")
+def getAllStops(train):
+    print("Sending to NJT:", {"token": token, "train": train})
+    response=requests.post("https://raildata.njtransit.com/api/TrainData/getTrainStopList",data={"token":token,"train":train})
+    print("Response status:", response.status_code)
+    print("Response text:", response.text[:200])
+    print("Token:", token[:10])  # just first 10 chars to verify token is there
+    if not response.text:
+        return jsonify({"error": "No data found"}), 404
+    return jsonify(response.json())
+
 
 if __name__=="__main__":
     app.run(debug=True)
